3d650f3b4a8d8a5950bb4344a71b65c331d33e7a
Priyanshu-Tyagi <manutyagi9991@gmail.com>
2023-07-13 00:10:03 +0530

    created signup with bcrypt

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,39 @@
 const express = require('express');
-const {connection} = require('./config/db')
+const { connection } = require('./config/db');
+const User = require('./model/user.model');
+const bcrypt = require('bcrypt');
 
 const app = express();
+app.use(express.json());
 
-app.get("/",(req,res) => {
+app.get("/", (req, res) => {
     res.send("Home Page")
 })
+
+app.post('/signup', async (req, res) => {
+    const { username, password } = req.body;
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+        res.status(400).send({ "error": 'Username already taken' });
+    } else {
+
+        bcrypt.hash(password, 4, async (err, hash) => {
+            if (err) {
+                res.status(500).send({ "err": 'something went wronge, please try again' });
+            }
+
+            const newuser = new User({ username, password: hash });
+            try {
+                await newuser.save();
+                res.send({ "msg": "Sign up successful" });
+            } catch (error) {
+                console.log('Error during signup: ', error)
+                res.status(500).send({ "error": 'Internal server error' })
+            }
+        })
+    }
+});
 
 app.listen(6996, async () => {
     try {
